bb163c02b5b9b0b71dceccf3abe86dae28f6cba8
Hasibur Rahman <hasiburbd24@gmail.com>
2022-10-17 20:31:10 +0600

    calling api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
+  const [countries, setCountries] = useState([])
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then((res) => res.json())
+      .then((data) => {
+        setCountries(data)
+        // console.log(data)
+      })
+      .catch((error) => console.log(error))
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Flags">
+          <h1>Loaded Country : {countries.length}</h1>
+          <h3>Countries Name With Flags</h3>
+          <ol className="FlagsList">
+            {countries.map((country) => (
+              <li>
+                {country.name.common} <br />{' '}
+                <img src={country.flags.png} alt="" />
+              </li>
+            ))}
+          </ol>
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
